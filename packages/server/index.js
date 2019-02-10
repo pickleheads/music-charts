e5ed8de4bb2c452a71f998c49596930d68c15fb9
Derek Grossman <derekgrossman@live.com>
2019-02-10 17:08:42 -0500

    added spotify api requests

--- packages/server/index.js
+++ packages/server/index.js
@@ -2,11 +2,34 @@
 
 const express = require('express');
 const db = require('./db');
+const fetch = require('node-fetch');
+require('dotenv').config();
+
+const { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET } = process.env;
 
 const app = express();
 
+(async () => {})();
+
 app.use(async (req, res, next) => {
   req.db = await db.connect();
+  const url = 'https://accounts.spotify.com/api/token';
+  const base64Str = Buffer.from(
+    `${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`
+  ).toString('base64');
+  const options = {
+    method: 'POST',
+    body: 'grant_type=client_credentials',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${base64Str}`,
+    },
+  };
+  const response = await fetch(url, options);
+  console.log({ OK: response.ok });
+  const json = await response.json();
+  console.log(JSON.stringify(json, null, 2));
+  req.spotifyAccessToken = json.access_token;
   next();
 });
 
@@ -21,8 +44,25 @@ app.get('/songs/:song', async (req, res) => {
   res.send();
 });
 
+app.get('/playlist/:playlistId', async (req, res) => {
+  const playlistId = req.params.playlistId;
+  const url = `https://api.spotify.com/v1/playlists/${playlistId}`;
+  const options = {
+    headers: {
+      Authorization: `Bearer ${req.spotifyAccessToken}`,
+    },
+  };
+  const response = await fetch(url, options);
+  console.log({ OK: response.ok });
+  const json = await response.json();
+  console.log(JSON.stringify(json, null, 2));
+  res.send(json);
+});
+
 app.use((req, res) => {
   res.send("Hey, you shouldn't be here you chicken chicken chicken");
 });
+
+//TODO: add error handling middleware
 
 app.listen(1337, () => console.log('Music charts is running on port 1337'));
